16077327280c27f3223a92940b0c4a091c42aef7
Tokenauts <Tokenauts@gmail.com>
2023-07-05 11:50:16 +0530

    changes in smarter collateral asset

--- components/SmarterCollateral.tsx
+++ components/SmarterCollateral.tsx
@@ -41,10 +41,17 @@ const SmarterCollateral = () => {
                 </div>
               </div>
               <div className="order-first block w-full mt-12 aspect-square lg:mt-0">
-                <img
+                {/* <img
                   className="object-cover object-center w-full mx-auto lg:ml-auto "
                   alt="hero"
                   src="DBR.svg"
+                /> */}
+                <video
+                  src="./col.mp4"
+                  className="object-cover object-center w-full mx-auto  lg:ml-auto lg:mt-32 "
+                  autoPlay
+                  muted
+                  loop
                 />
               </div>
             </div>
