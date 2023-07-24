73527bffb839f8816180b05a4aa793035accc137
Tokenauts <Tokenauts@gmail.com>
2023-07-24 14:23:33 +0530

    update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="relative flex items-center w-full ">
       <div className="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
         <video
-          src="./security.mp4"
+          src="./Security.mp4"
           className="object-cover rounded-xl w-full h-96 "
           autoPlay
           muted
