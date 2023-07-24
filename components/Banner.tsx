c3e63cc8dfe860e0eb91614a62ff6bfc4877e3a3
Tokenauts <Tokenauts@gmail.com>
2023-07-24 15:30:08 +0530

    update in banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="relative flex items-center w-full ">
-      <div className="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
+      <div className="relative items-center w-full  py-24 mx-auto ">
         <video
           src="./security.mp4"
           className="object-cover rounded-xl w-full h-96 "
