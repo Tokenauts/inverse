d3ac29bcd886710506f26d81f7971a2ca6ff97ad
Tokenauts <Tokenauts@gmail.com>
2023-07-29 15:48:13 +0530

    update in switch feature

--- components/SwitchFeature.tsx
+++ components/SwitchFeature.tsx
@@ -13,89 +13,90 @@ const SwitchFeature: React.FC = () => {
   };
 
   return (
-    <div className="relative items-center w-full px-5 lg:py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
-      <h1 className="lg:text-6xl text-white font-black text-4xl  ">
-        Our Ecosystem
-      </h1>
-      <div className="inline-flex border items-center mt-10 text-white  rounded-3xl cursor-pointer dark:text-white mx-auto     lg:px-4 py-2">
-        <span
-          onClick={() => handleTabClick("DEX")}
-          className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs   ${
-            selectedTab === "DEX"
-              ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
-              : " text-w"
-          }`}
-        >
-          DEX
-        </span>
+    <div className="relative items-center w-full  gap-4 px-5 lg:py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
+      <img
+        src="grid.svg"
+        className="absolute opacity-60 mix-blend-plus-lighter w-full h-full "
+        style={{ zIndex: -1 }}
+      />
+      <div className="grid grid-cols-2 z-10" style={{ zIndex: 2 }}>
+        <h1 className="lg:text-6xl text-white font-black text-4xl mt-8   ">
+          Our Ecosystem
+        </h1>
+        <div className="inline-flex border items-center mt-10 text-white  rounded-3xl cursor-pointer mx-auto px-2 bg-gray-700     lg:px-4 py-2">
+          <span
+            onClick={() => handleTabClick("DEX")}
+            className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs    ${
+              selectedTab === "DEX"
+                ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
+                : " text-w"
+            }`}
+          >
+            DEX
+          </span>
 
-        <span
-          onClick={() => handleTabClick("CEX")}
-          className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs  ${
-            selectedTab === "CEX"
-              ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
-              : " text-w"
-          }`}
-        >
-          CEX
-        </span>
-        <span
-          onClick={() => handleTabClick("YIELD")}
-          className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs ${
-            selectedTab === "YIELD"
-              ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
-              : " text-w"
-          }`}
-        >
-          Yield
-        </span>
-        <span
-          onClick={() => handleTabClick("INTERFACE")}
-          className={`lg:px-4 lg:py-2 rounded-full px-2  text-xs ${
-            selectedTab === "INTERFACE"
-              ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
-              : "text-w"
-          }`}
-        >
-          Interface
-        </span>
-        <span
-          onClick={() => handleTabClick("WALLETS")}
-          className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs ${
-            selectedTab === "WALLETS"
-              ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
-              : "text-w"
-          }`}
-        >
-          Wallets
-        </span>
+          <span
+            onClick={() => handleTabClick("CEX")}
+            className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs  ${
+              selectedTab === "CEX"
+                ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
+                : " text-w"
+            }`}
+          >
+            CEX
+          </span>
+          <span
+            onClick={() => handleTabClick("YIELD")}
+            className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs ${
+              selectedTab === "YIELD"
+                ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
+                : " text-w"
+            }`}
+          >
+            Yield
+          </span>
+          <span
+            onClick={() => handleTabClick("INTERFACE")}
+            className={`lg:px-4 lg:py-2 rounded-full px-2  text-xs ${
+              selectedTab === "INTERFACE"
+                ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
+                : "text-w"
+            }`}
+          >
+            Interface
+          </span>
+          <span
+            onClick={() => handleTabClick("WALLETS")}
+            className={`lg:px-4 lg:py-2 rounded-full px-2 text-xs ${
+              selectedTab === "WALLETS"
+                ? " bg-violet-500 border text-white font-light rounded-full text-xs lg:text-base"
+                : "text-w"
+            }`}
+          >
+            Wallets
+          </span>
+        </div>
       </div>
       {selectedTab === "DEX" && (
         <div>
-          <div className="grid grid-cols-3 gap-4 p-5 mx-auto max-w-2xl">
+          <div className="grid grid-cols-3 gap-4 p-5  max-w-xl mx-auto ">
             <div className="shadow-lg text-lg font-bold text-center  p-10 rounded-lg row-span-1">
               <img src="./DEX-01.svg" className="  " />
-              <p className="text-white mt-2">Balancer</p>
             </div>
             <div className="shadow-lg text-lg font-bold text-center p-10 rounded-lg">
               <img src="./DEX-02.svg" className=" " />
-              <p className="text-white mt-2">Velodrome</p>
             </div>
             <div className="shadow-lg text-lg font-bold text-center p-10 rounded-lg row-span-1">
               <img src="./DEX-03.svg" className="" />
-              <p className="text-white mt-2">Convex</p>
             </div>
             <div className="shadow-lg  text-lg font-bold text-center p-10 rounded-lg">
               <img src="./DEX-04.svg" className=" " />
-              <p className="text-white mt-2">Uniswap</p>
             </div>
             <div className="shadow-lg  text-lg font-bold text-center p-10 rounded-lg col-span-1">
               <img src="./DEX-05.svg" className=" " />
-              <p className="text-white mt-2">SuishiSwap</p>
             </div>
             <div className="shadow-lg  text-lg font-bold text-center p-10 rounded-lg col-span-1">
               <img src="./DEX-06.svg" className="" />
-              <p className="text-white mt-2">Athena</p>
             </div>
           </div>
         </div>
